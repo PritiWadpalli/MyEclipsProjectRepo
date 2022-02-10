ea4bd361aa7900a4b6efbeda2d02021f47dd0609
Priti <shriramwar.priti@gmail.com>
2022-02-10 16:31:24 +0530

    first test case updated

---
+++ src/test/java/testcases/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void welcomeMsg()
+	{
+		System.out.println("Welcome to TestNG testCases");
+	}
+	
+	
+}
